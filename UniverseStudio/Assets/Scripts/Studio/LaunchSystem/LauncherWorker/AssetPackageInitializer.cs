113abe20fd0e3aa5279d50bd6a17c2d094654a5f
buxinyuan <935160739@qq.com>
2023-03-14 00:28:30 +0800

    重构AssetSystem step 1

--- UniverseStudio/Assets/Scripts/Studio/LaunchSystem/LauncherWorker/AssetPackageInitializer.cs
+++ UniverseStudio/Assets/Scripts/Studio/LaunchSystem/LauncherWorker/AssetPackageInitializer.cs
@@ -16,29 +16,33 @@ namespace UniverseStudio
         protected override void OnStart()
         {
             m_PackageState[AssetInitializeParam.UI_PACKAGE] = false;
-            Engine.CreateAssetsPackage(AssetInitializeParam.UI_PACKAGE);
-            Engine.SetUIPackageName(AssetInitializeParam.UI_PACKAGE);
-
-            if (Engine.GetAssetsPackage(AssetInitializeParam.UI_PACKAGE, out AssetsPackage uiPackage))
-            {
-                Engine.StartGlobalCoroutine(InitializePackage(AssetInitializeParam.UI_PACKAGE, uiPackage));
-            }
-            
-            Engine.CreateAssetsPackage(AssetInitializeParam.SCENE_PACKAGE);
-            Engine.SetScenePackageName(AssetInitializeParam.SCENE_PACKAGE);
-
-            if (Engine.GetAssetsPackage(AssetInitializeParam.SCENE_PACKAGE, out AssetsPackage scenePackage))
-            {
-                Engine.StartGlobalCoroutine(InitializePackage(AssetInitializeParam.SCENE_PACKAGE, scenePackage));
-            }
+            // Engine.CreateAssetsPackage(AssetInitializeParam.UI_PACKAGE);
+            // Engine.SetUIPackageName(AssetInitializeParam.UI_PACKAGE);
+            //
+            // if (Engine.GetAssetsPackage(AssetInitializeParam.UI_PACKAGE, out AssetsPackage uiPackage))
+            // {
+            //     Engine.StartGlobalCoroutine(InitializePackage(AssetInitializeParam.UI_PACKAGE, uiPackage));
+            // }
+            //
+            // Engine.CreateAssetsPackage(AssetInitializeParam.SCENE_PACKAGE);
+            // Engine.SetScenePackageName(AssetInitializeParam.SCENE_PACKAGE);
+            //
+            // if (Engine.GetAssetsPackage(AssetInitializeParam.SCENE_PACKAGE, out AssetsPackage scenePackage))
+            // {
+            //     Engine.StartGlobalCoroutine(InitializePackage(AssetInitializeParam.SCENE_PACKAGE, scenePackage));
+            // }
         }
 
         IEnumerator InitializePackage(string packageName, AssetsPackage package)
         {
-            HostPlayModeParameters param = AssetInitializeParam.HostPlay;
-            InitializationOperation operation = package.InitializeAsync(param);
-            yield return operation;
+            //TODO LOG MASK
+            //TODO 重构一遍AssetSystem, 池化一些对象
+            //将一些接口内置到Engine中，外部只暴露InitializeAssetPackage即可, 提供所有AssetPackage的进度，方便UI显示
+            OfflinePlayModeParameters param = AssetInitializeParam.OfflineParams();
+            // InitializationOperation operation = package.InitializeAsync(param);
+            // yield return operation;
             m_PackageState[packageName] = package.InitializeStatus == EOperationStatus.Succeed;
+            yield break;
         }
     }
 }
